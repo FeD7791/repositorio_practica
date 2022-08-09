5db08fd4a83fc92034c8354d5acc82edbca27ed2
FeD7791 <108427440+FeD7791@users.noreply.github.com>
2022-08-09 18:44:20 -0300

    se_agrega_una_tercer_variable

--- clase8/app3/CRUD/return.js
+++ clase8/app3/CRUD/return.js
@@ -3,6 +3,7 @@ function returnn(tasks,titulo){
     titulos coincidan con la palabra titulo*/
     function returner(arg){return arg.title === titulo}//funcion para ser utilizada como condicion para el metodo filter
         const tareas = tasks.filter(returner)//regresa un arreglo con los objetos cuyo titulo coincidan con titulo
+        const variable = 'esta es otra variable de pureba 3';
         
         if(tareas.length >0){
             /*Este if es necesario por que tareas puede ser un arreglo vacio  */
